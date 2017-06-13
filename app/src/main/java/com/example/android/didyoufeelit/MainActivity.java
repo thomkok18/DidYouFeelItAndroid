023e68a5e3eba058e5933474d5281cfd615800ab
Thom Kok <tkok@roc-dev.com>
2017-06-13 15:40:12 +0200

    update

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -11,7 +11,9 @@ import android.widget.TextView;
  */
 public class MainActivity extends AppCompatActivity {
 
-    /** URL for earthquake data from the USGS dataset */
+    /**
+     * URL for earthquake data from the USGS dataset
+     */
     private static final String USGS_REQUEST_URL =
             "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
 
@@ -45,13 +47,18 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected Event doInBackground(String... urls) {
-
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
             // Perform the HTTP request for earthquake data and process the response.
             Event result = Utils.fetchEarthquakeData(urls[0]);
             return result;
         }
 
         protected void onPostExecute(Event result) {
+            if (result == null) {
+                return;
+            }
             updateUi(result);
         }
     }
